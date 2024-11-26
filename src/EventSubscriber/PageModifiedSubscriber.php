7a02141fb4218659c2c39abed8ce8769d6ce42a2
Łukasz Kilijański <lukasz.kilijanski.programista@gmail.com>
2024-11-26 15:21:06 +0100

    subscriber adjustments

--- src/EventSubscriber/PageModifiedSubscriber.php
+++ src/EventSubscriber/PageModifiedSubscriber.php
@@ -1,26 +1,14 @@
 <?php
 declare(strict_types=1);
 
-/**
- * File: PageModifiedSubscriber.php
- *
- * @author Łukasz Kilijański <lukasz.kilijanski@smartive.app>
- * @copyright Copyright (C) 2024 smartive.app (https://smartive.app)
- */
-
 namespace App\EventSubscriber;
 
-use App\Entity\Car;
-use Doctrine\ORM\EntityManagerInterface;
 use Sulu\Component\DocumentManager\Event\PersistEvent;
 use Sulu\Component\DocumentManager\Events;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class PageModifiedSubscriber implements EventSubscriberInterface
 {
-
-    public function __construct(private EntityManagerInterface $entityManager) {}
-
     public static function getSubscribedEvents()
     {
         {
